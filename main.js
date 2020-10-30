a3e4ee2764cf38defc5610a27352003d54a650cb
Dissmind <dissmind@gmail.com>
2020-10-30 16:11:47 +0200

    new

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
-const tgBot = require('node-telegram-bot-api')
 const os = require('os')
 
-const token = '1233073904:AAGw1i5AV1Yevns9zhqJJgvKy3aoPNxN9yw'
-const bot = new tgBot(token, {polling: true})
 
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Hello from ${os.type()}`)
+
+
+const { Telegraf } = require('telegraf');
+const covidApi = require('covid19-api');
+const token = '1233073904:AAG2i4iPkhINYmTZQH_PHJHHdoJBDlHvVgw'
+
+// const COUNTRIES_LIST = require('./const')
+const bot = new Telegraf(token)
+
+
+bot.start( ctx => ctx.reply(`
+   Привет ${ctx.from.first_name}!
+   Узнай статистику по Коронавирусу.
+   Введи страну на английском языке и получи статистику.
+   Получить весь список стран можно по команде /help."
+   Hello from ${os.type()}!
+`))
+
+bot.help( ctx => ctx.reply(COUNTRIES_LIST)) // список всех стран на английском языке можно взять в документации covid19-api
+
+bot.on('text', async (ctx) => {
+    try {
+        const userText = ctx.message.text
+        const covidData = await covidApi.getReportsByCountries(userText)
+        const countryData = covidData[0][0]
+        const formatData = `
+           Страна: ${countryData.country},
+           Случаи: ${countryData.cases},
+           Смерти: ${countryData.deaths},
+           Выздоровело: ${countryData.recovered}`
+        ctx.reply(formatData)
+    } catch(e) {
+        ctx.reply('Такой страны не существует, для получения списка стран используй команду /help')
+    }
 })
+
+bot.launch()
